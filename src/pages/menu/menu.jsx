6a7addadeb3733a20af3c52054bbf7156fd83ba2
Pepe Martinez <pepemart1996@gmail.com>
2020-08-10 06:59:52 -0600

    init menu page

---
+++ src/pages/menu/menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class MenuPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                hola mundo
+            </div>
+        )
+    }
+}

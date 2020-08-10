c292e9ea7699074c50b8fe6ce95555c61e46296a
Pepe Martinez <pepemart1996@gmail.com>
2020-08-10 07:34:30 -0600

    Menu input y boton

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import './menu.css'
 
 export class MenuPage extends React.Component {
 
     render() {
         return (
-            <div>
-                hola mundo
+            <div className="menu-page">
+                <div className="header">
+                    <div>
+                        Conquian
+                    </div>
+                </div>
+                <div className="menu-container">
+                    <div>
+                        <div className="input-name-container">
+                            <input placeholder="Ingresa tu nombre..." className="name-input">
+                            </input>
+                        </div>
+                        <div className="button-play-container">
+                            <button className="button-play">Jugar</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
